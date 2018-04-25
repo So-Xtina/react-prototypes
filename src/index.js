33194795926ee4ca362fa6bb5ebb79fe043eb332
So-Xtina <mu7zic2soul@gmail.com>
2018-04-24 18:16:02 -0700

    lucky number exercise completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    var randomNum =  Math.floor(Math.random() * 1000);
+    return randomNum;
+}
+
+var user = {
+    name: 'Christina',
+    luckyNumber: luckyNumber()
+};
+
+function greeting(user){
+    return  <div class="container">
+                <h1>Hello! {user.name}</h1>
+                <h2 class="text-muted">The lucky number is: {user.luckyNumber}</h2>
+            </div>
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
