e52abe7adf8c02b354592e02db3d37cff908bcf7
So-Xtina <mu7zic2soul@gmail.com>
2018-04-28 12:47:29 -0700

    created a student grade table with JSX and components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,29 @@
+import React from "react";
+import Table from "./table";
 
+const students = [{
+    student: 'Bruce Wayne',
+    course: 'Criminal Justice',
+    grade: 94
+}, 
+{
+    student: 'Barry Allen',
+    course: 'Forensic Science',
+    grade: 98
+}, 
+{
+    student: 'Clark Kent',
+    course: 'Creative Writing',
+    grade: 86
+}];
 
+function App() {
+	return (
+		<div className="container">
+			<h1>Student Grade Table</h1>
+			<Table data={students}/>
+		</div>
+	);
+}
+
+export default App;
