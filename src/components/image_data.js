408e215d6dff2a83ed257ae3c127aa4db58a60c9
So-Xtina <mu7zic2soul@gmail.com>
2018-05-03 21:04:08 -0700

    styles and images completed for the scrapbook page

---
+++ src/components/image_data.js
@@ -0,0 +1,72 @@
+import doveOrchid from "./images/dove_orchid.jpg";
+import dahlia from "./images/dahlia.JPG";
+import hibiscus from "./images/hibiscus.jpg";
+import hydrangea from "./images/hydrangea.jpg";
+import lilacs from "./images/lilacs.jpg";
+import lotus from "./images/lotus.jpg";
+import pansies from "./images/pansies.jpeg";
+import tulips from "./images/tulips.jpg";
+
+export default [
+	{
+		src: doveOrchid,
+		style: {
+			left: "5%",
+			transform: "rotate(-10deg)"
+		}
+	},
+	{
+		src: dahlia,
+		style: {
+			left: "40%",
+			transform: "rotate(6deg)"
+		}
+	},
+	{
+		src: hibiscus,
+		style: {
+			right: "15%",
+			transform: "rotate(-9deg)"
+		}
+	},
+	{
+		src: hydrangea,
+		style: {
+			left: "8%",
+			top: "28%",
+			transform: "rotate(8deg)"
+		}
+	},
+	{
+		src: lilacs,
+		style: {
+			left: "37%",
+			top: "25%",
+			transform: "rotate(-12deg)"
+		}
+	},
+	{
+		src: lotus,
+		style: {
+			right: "12%",
+			top: "26%",
+			transform: "rotate(10deg)"
+		}
+	},
+	{
+		src: pansies,
+		style: {
+			left: "25%",
+			top: "48%",
+			transform: "rotate(-5deg)"
+		}
+	},
+	{
+		src: tulips,
+		style: {
+			right: "25%",
+			top: "52%",
+			transform: "rotate(16deg)"
+		}
+	}
+];
---
+++ src/components/scrapbook.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./scrapbook.css";
+import imageData from "./image_data";
+import ScrapbookImage from "./scrapbook_images";
+
+export default props => {
+	const images = imageData.map((img, index) => {
+		return <ScrapbookImage key={index} about={img} />;
+	});
+
+	return <div className="scrapbook-container">{images}</div>;
+};
