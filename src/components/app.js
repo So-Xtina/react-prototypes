209a154fc53aa1f15471e2fe9396d8017df0f3d1
So-Xtina <mu7zic2soul@gmail.com>
2018-05-09 10:50:29 -0700

    implemented the clock with redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from "react";
+import Clock from "./clock";
 
+function App() {
+	return (
+		<div className="container">
+			<h1 className="text-center">Clock Work in Redux!</h1>
+			<Clock />
+		</div>
+	);
+}
 
+export default App;
